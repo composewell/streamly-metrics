a7ac167924674d939abc1ffbfc928aa89f13f625
Harendra Kumar <harendra@composewell.com>
2021-10-23 23:47:44 +0530

    Refactor, move the metrics type to a lower module

--- src/Streamly/Metrics/Perf.hs
+++ src/Streamly/Metrics/Perf.hs
@@ -5,107 +5,22 @@ module Streamly.Metrics.Perf
     )
 where
 
-import Data.Word (Word64)
 import GHC.Stats (getRTSStats, getRTSStatsEnabled, RTSStats(..))
-import Streamly.Metrics.Type (GaugeMax(..), Seconds(..), Bytes(..))
 import Streamly.Metrics.Measure (bracketWith)
+import Streamly.Metrics.Perf.Type (PerfMetrics(..))
 import Streamly.Metrics.RUsage (RUsage(..), pattern RUsageSelf, getRUsage)
-import System.Mem (performGC)
+import Streamly.Metrics.Type (GaugeMax(..), Seconds(..), Bytes(..))
 import System.CPUTime (getCPUTime)
+import System.Mem (performGC)
 
--- Use Counter/Gauge as the outer constructor and Bytes/Seconds as the inner
--- constuctor.
-data Stats =
-    CPUTime !(Seconds Double)
-
-    -- GC Stats
-  | GcAllocatedBytes !(Bytes Word64)
-  | GcCopiedBytes !(Bytes Word64)
-  | GcMaxMemInUse !(GaugeMax (Bytes Word64))
-
-    -- rusage Stats
-  | RuUtime     !(Seconds Double)
-  | RuStime     !(Seconds Double)
-  | RuMaxrss    !(GaugeMax (Bytes Word64))
-  | RuIxrss     !(GaugeMax (Bytes Word64))
-  | RuIdrss     !(GaugeMax (Bytes Word64))
-  | RuIsrss     !(GaugeMax (Bytes Word64))
-  | RuMinflt    !Word64
-  | RuMajflt    !Word64
-  | RuNswap     !Word64
-  | RuInblock   !Word64
-  | RuOublock   !Word64
-  | RuMsgsnd    !Word64
-  | RuMsgrcv    !Word64
-  | RuNsignals  !Word64
-  | RuNvcsw     !Word64
-  | RuNivcsw    !Word64
-    deriving (Show)
-
-#define UNARY_OP_ONE(constr,op) op (constr a) = constr (op a)
-#define UNARY_OP(op) \
-    UNARY_OP_ONE(CPUTime,op); \
-    UNARY_OP_ONE(GcAllocatedBytes,op); \
-    UNARY_OP_ONE(GcCopiedBytes,op); \
-    UNARY_OP_ONE(RuUtime,op); \
-    UNARY_OP_ONE(RuStime,op); \
-    UNARY_OP_ONE(RuMaxrss,op); \
-    UNARY_OP_ONE(RuIxrss,op); \
-    UNARY_OP_ONE(RuIdrss,op); \
-    UNARY_OP_ONE(RuIsrss,op); \
-    UNARY_OP_ONE(RuMinflt,op); \
-    UNARY_OP_ONE(RuMajflt,op); \
-    UNARY_OP_ONE(RuNswap,op); \
-    UNARY_OP_ONE(RuInblock,op); \
-    UNARY_OP_ONE(RuOublock,op); \
-    UNARY_OP_ONE(RuMsgsnd,op); \
-    UNARY_OP_ONE(RuMsgrcv,op); \
-    UNARY_OP_ONE(RuNsignals,op); \
-    UNARY_OP_ONE(RuNvcsw,op); \
-    UNARY_OP_ONE(RuNivcsw,op);
-
-#define INFIX_OP_ONE(constr,op) constr a op constr b = constr (a op b)
-#define FUNC_OP_ONE(constr,op) constr a `op` constr b = constr (a `op` b)
-
-#define INFIX_OP(op) \
-    INFIX_OP_ONE(CPUTime,op); \
-    INFIX_OP_ONE(GcAllocatedBytes,op); \
-    INFIX_OP_ONE(GcCopiedBytes,op); \
-    INFIX_OP_ONE(GcMaxMemInUse,op); \
-    INFIX_OP_ONE(RuUtime,op); \
-    INFIX_OP_ONE(RuStime,op); \
-    INFIX_OP_ONE(RuMaxrss,op); \
-    INFIX_OP_ONE(RuIxrss,op); \
-    INFIX_OP_ONE(RuIdrss,op); \
-    INFIX_OP_ONE(RuIsrss,op); \
-    INFIX_OP_ONE(RuMinflt,op); \
-    INFIX_OP_ONE(RuMajflt,op); \
-    INFIX_OP_ONE(RuNswap,op); \
-    INFIX_OP_ONE(RuInblock,op); \
-    INFIX_OP_ONE(RuOublock,op); \
-    INFIX_OP_ONE(RuMsgsnd,op); \
-    INFIX_OP_ONE(RuMsgrcv,op); \
-    INFIX_OP_ONE(RuNsignals,op); \
-    INFIX_OP_ONE(RuNvcsw,op); \
-    INFIX_OP_ONE(RuNivcsw,op); \
-    _ op _ = error "Cannot operate on different types of metrics";
-
--- XXX Can we derive this generically?
-instance Num Stats where
-    UNARY_OP(signum)
-    UNARY_OP(abs)
-    INFIX_OP(+)
-    INFIX_OP(-)
-    INFIX_OP(*)
-
-getProcMetrics :: IO [Stats]
+getProcMetrics :: IO [PerfMetrics]
 getProcMetrics = do
     cpuPico <- getCPUTime
     let cpuSec = fromIntegral cpuPico / (10^(12 :: Int))
     return [CPUTime cpuSec]
 
 -- Compatible with GHC 8.2 (base 4.10) onwards
-getGcMetrics :: IO [Stats]
+getGcMetrics :: IO [PerfMetrics]
 getGcMetrics = do
     res <- getRTSStatsEnabled
     if res
@@ -118,7 +33,7 @@ getGcMetrics = do
             ]
     else pure []
 
-getRuMetrics :: IO [Stats]
+getRuMetrics :: IO [PerfMetrics]
 getRuMetrics = do
     ru <- getRUsage RUsageSelf
     return
@@ -140,26 +55,26 @@ getRuMetrics = do
         , RuNivcsw   (ru_nivcsw ru)
         ]
 
-getPerfMetrics :: IO [Stats]
+getPerfMetrics :: IO [PerfMetrics]
 getPerfMetrics = do
     procMetrics <- getProcMetrics
     gcMetrics <- getGcMetrics
     ruMetrics <- getRuMetrics
     return $ procMetrics ++ gcMetrics ++ ruMetrics
 
-preRun :: IO [Stats]
+preRun :: IO [PerfMetrics]
 preRun = do
   performGC
   getPerfMetrics
 
-postRun :: [Stats] -> IO [Stats]
+postRun :: [PerfMetrics] -> IO [PerfMetrics]
 postRun stats = do
   performGC
   stats1 <- getPerfMetrics
   return $ zipWith (-) stats1 stats
 
-benchWith :: (a -> IO b) -> a -> IO (b, [Stats])
+benchWith :: (a -> IO b) -> a -> IO (b, [PerfMetrics])
 benchWith = bracketWith preRun postRun
 
-bench :: IO a -> IO (a, [Stats])
+bench :: IO a -> IO (a, [PerfMetrics])
 bench action = benchWith (const action) ()
