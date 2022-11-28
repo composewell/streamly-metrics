1a7409bd06a417457d2650c2a06b11aa4212cdf5
Ranjeet Kumar Ranjan <ranjeetr@composewell.com>
2022-11-28 20:01:09 +0530

    Fix build

--- src/Streamly/Metrics/Console.hs
+++ src/Streamly/Metrics/Console.hs
@@ -5,7 +5,5 @@ where
 
 import Streamly.Metrics.Type
 import Streamly.Internal.Data.Tuple.Strict (Tuple'(..))
-import Streamly.Internal.Data.Array.Unboxed (Array)
+import Streamly.Internal.Data.Array (Array)
 import Streamly.Internal.Data.Time.Units (AbsTime)
-import Data.Word (Word64)
-
